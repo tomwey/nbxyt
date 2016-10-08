d41062b5b8e95baf34d9ae7b7bf2eab2a6675933
Tomwey <tomwey@163.com>
2016-10-08 20:52:13 +0800

    fixed bug

--- db/migrate/20161008064132_change_columns_for_companies.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnsForCompanies < ActiveRecord::Migration
-  def change
-    rename_column :companies, :mentor_id, :user_id
-    add_column :companies, :body, :text, null: false
-  end
-end
